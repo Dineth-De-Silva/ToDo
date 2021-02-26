198c89d9a4306558926295204ef8a0b63b167b4b
Dineth-De-Silva <dadinethdesilva@gmail.com>
2021-02-26 14:42:35 +0530

    Test

--- JS/Code.js
+++ JS/Code.js
@@ -1,3 +1,18 @@
+window.addEventListener("load", (e) => {
+  new PWAConfApp();
+  registerSW();
+});
+async function registerSW() {
+  if ("serviceWorker" in navigator) {
+    try {
+      await navigator.serviceWorker.register("../sw.js");
+    } catch (e) {
+      alert("ServiceWorker registration failed. Sorry about that.");
+    }
+  } else {
+    document.querySelector(".alert").removeAttribute("hidden");
+  }
+}
 var Lasttodono = 0;
 function Reloadtodoos() {
   firebase
